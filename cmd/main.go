9fac80263168d1652360ae02bc681a9e6a6d5a0f
Luiz Gustavo Palmieri <lgustavopalmieri@gmail.com>
2024-09-12 20:42:27 -0300

    refactor: trying to bind clusters

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "Ultrafactory OEE on Kubernetees!!")
+}
 
 func main() {
-	fmt.Println("ultrafactory oee main branch created")
-	fmt.Println("testing pr template")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:4003", nil))
 }
